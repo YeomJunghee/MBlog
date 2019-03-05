1529656c8671a78366f24c98b69c2c432cfe3c0d
Elia <dua1791@naver.com>
2019-03-05 15:58:52 +0900

    se

--- second/accounts/views.py
+++ second/accounts/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.shortcuts import redirect
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+            auth.login(request, user)
+            return redirect('signup')
+    return render(request, 'signup.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('signup')
+        else:
+            return render(request, 'login.html', {'error': 'username or password is incorrect'})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('signup')
+    return render(request, 'login.html')
